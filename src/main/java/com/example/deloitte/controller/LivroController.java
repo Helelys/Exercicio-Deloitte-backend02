d3338c9534b0f6fae4e77e01cc746171450dcdc1
bootcamp01 <bootcamp01@deloitte.com>
2025-05-09 14:00:49 -0300

    Enviando para repositório

---
+++ src/main/java/com/example/deloitte/controller/LivroController.java
@@ -0,0 +1,31 @@
+package com.example.deloitte.controller;
+
+import com.example.deloitte.model.entity.Livro;
+import com.example.deloitte.repository.LivroRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/livros")
+@AllArgsConstructor
+public class LivroController {
+
+    private LivroRepository livroRepository;
+
+    @GetMapping("/exibir")
+    public List<Livro> listarLivros() {
+       return livroRepository.findAll();
+    }
+
+    @PostMapping("/adicionar")
+    public Livro adicionarLivro(@RequestBody Livro livro) {
+        return livroRepository.save(livro);
+    }
+    @DeleteMapping("/{id}")
+    public void removerLivro(@PathVariable Long id) {
+        livroRepository.deleteById(id);
+    }
+}
